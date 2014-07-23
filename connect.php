6c05d51689839296acf940bf5acef2b99d04d2ae
r.klukvin <w002l109a0>
2014-07-23 13:18:05 +0400

    functions for database was added

--- connect.php
+++ connect.php
@@ -5,42 +5,35 @@
 <body>
 <?
 include("PR.php");
-$db_host = 'localhost';
-$db_user = 'root';
-$db_password = '';
-$db_name = 'Questions';
-
-$link = mysqli_connect($db_host, $db_user, $db_password, $db_name);
-if (!$link) {
-    die('<p style="color:red">'.mysqli_connect_errno().' - '.mysqli_connect_error().'</p>');
-}
-mysqli_query($link, "SET NAMES utf8");
-
-function getUnansweredQuestions() {
-    if ($result = mysqli_query($link, "SELECT Name FROM City LIMIT 10")) {
-        printf("Select вернул %d строк.\n", mysqli_num_rows($result));
-
-        /* очищаем результирующий набор */
-        mysqli_free_result($result);
-    }
-}
 ?>
 
 <?
     class DBQuestions {
         private static $mysqli;
+
         public static function connect(){
             self::$mysqli = new mysqli("localhost", "root", "", "Questions");
             self::$mysqli->set_charset("utf8");
         }
+
+        public static function addQuestion($text, $name) {
+            $date = date('Y-m-d H:i:s');
+            $status = false;
+            $id = null;
+            self::$mysqli->query("INSERT INTO questions (`ID`, `Text`, `Date`, `Status`, `Name`) VALUES ('$id', '$text', '$date', '$status', '$name')");
+        }
+
         public static function getUnansweredQuestions(){
-            if ($result = self::$mysqli->query("SELECT * FROM  questions WHERE STATUS =0 ORDER BY DATE")) {
+            if ($result = self::$mysqli->query("SELECT * FROM  questions WHERE STATUS = 0 ORDER BY DATE")) {
                return $result->fetch_all();
             }
         }
+
+        public static function changeActiveQuestionStatus() {
+            self::$mysqli->query("UPDATE questions SET Status = 1 WHERE STATUS = 0 ORDER BY DATE LIMIT 1");
+        }
     }
     DBQuestions::connect();
-    DBQuestions::getUnansweredQuestions();
 ?>
 </body>
 </html>
