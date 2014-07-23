81d45006b6a60423ebe55655597d332eda3e3d45
nifontov <nifontov.s@list.ru>
2014-07-23 10:55:49 +0300

    Add .htaccess file which allow to interpret php code in html documents. Add maket of current questions page. Made decomposition of page into links, header, work area and footer

--- index.php
+++ index.php
@@ -4,36 +4,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Question</title>
-
-    <link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
-
-    <link href="css/main.css" rel="stylesheet" >
-    <script src="js/adderQuestion.js"></script>
+    <?php include 'links.php' ?>
 </head>
 
 <body>
-<header>
-    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="/"><span class="logo">Questions</span></a>
-            </div>
-            <div class="collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="/">Задать вопрос</a></li>
-                    <li><a href="/questions.html">Текущие вопросы</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-</header>
+<?php include 'header.php' ?>
 <main class="container">
     <div class="page-header">
         <h1>Новый вопрос</h1>
@@ -52,9 +27,5 @@
         </div>
     </div>
 </main>
-<footer class="footer">
-    <div class="container">
-        <span class="logo">Questions</span>
-    </div>
-</footer>
+<?php include 'footer.php' ?>
 </body></html>
---
+++ links.php
@@ -0,0 +1,6 @@
+<link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
+
+<link href="css/main.css" rel="stylesheet" >
+<script src="js/adderQuestion.js"></script>
