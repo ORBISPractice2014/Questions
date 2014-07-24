871525518f8de1898167d7101e64c3aa10186734
LonelyGriffin <LonelyGriffin@yandex.ru>
2014-07-24 14:04:25 +0400

    footer update
    Добавлены авторы.

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer">
     <div class="container">
-        <span class="logo">Questions</span>
+        <span class="logo">Questions</span><span class="small" style="font-style: italic;"> by Fanaskov Pavel, Klukvin Roman, Sergei Nifontov</span>
     </div>
 </footer>
