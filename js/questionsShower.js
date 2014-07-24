7b3885319f80034725b217b14c1d63271bf342d1
nifontov <nifontov.s@list.ru>
2014-07-24 14:14:22 +0300

    Add message indicated that there are no unanswered questions in page with current questions

--- js/questionsShower.js
+++ js/questionsShower.js
@@ -4,22 +4,37 @@ $(document).ready(function() {
                 var data=JSON.parse(data),
                     questions = data['questions'],
                     curentQuestion = $('.currentQuestion'),
-                    questionQuery = $('.questionQuery');
+                    questionQuery = $('.questionQuery'),
+                    message=$('.message');
 
-                curentQuestion.find('.name').html('<strong>'+questions[0][3]+ '</strong>');
-                curentQuestion.find('.time').html(questions[0][1]);
-                curentQuestion.find('.panel-body').html('<b>'+questions[0][0]+'</b>');
-                questionQuery.empty();
-                for(var i=1;i<questions.length;i++) {
-                    questionQuery.append('<div class="panel panel-info">'+
-                                            '<div class="panel-heading"> <div class="name" style="float: left"> <strong>'+questions[i][3]+'</strong> </div> <div class="time" style="margin-left:20%"><small> '+questions[i][1]+' </small> </div>'+
+                if(questions.length!=0) {
+                     curentQuestion.html('<div class="panel panel-primary" >'+
+                                            '<div class="panel-heading">'+
+                                                '<h3 class="panel-title"><div class="name" style="float: left"><strong>'+questions[0][3]+ '</strong></div> <div class="time" style=" font-color:rgb(255,255,255); margin-left:20%;">'+ questions[0][1]+' </div></h3>'+
                                             '</div>'+
                                             '<div class="panel-body">'+
-                                                 '<b>'+questions[i][0]+' </b>'+
+                                                '<b>'+questions[0][0]+'</b>'+
                                             '</div>'+
-                                        '</div>');
+                                        '</div> ');
+                    questionQuery.empty();
+                    for(var i=1;i<questions.length;i++) {
+                        questionQuery.append('<div class="panel panel-info">'+
+                                                '<div class="panel-heading"> <div class="name" style="float: left"> <strong>'+questions[i][3]+'</strong> </div> <div class="time" style="margin-left:20%"><small> '+questions[i][1]+' </small> </div>'+
+                                                '</div>'+
+                                                '<div class="panel-body">'+
+                                                     '<b>'+questions[i][0]+' </b>'+
+                                                '</div>'+
+                                            '</div>');
 
+                    }
+                    message.empty();
                 }
+                else {
+                    message.html('<div class="alert alert-info">'+
+                    'На данный момент нет неотвеченных вопросов.'+
+                    '</div>');
+                }
+
 
 
 
