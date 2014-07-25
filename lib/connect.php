08b3b95eb2d861d8de984df88c7a0d26ebacb8bc
r.klukvin <w002l109a0>
2014-07-25 12:33:03 +0400

    DB functions was modified

--- lib/connect.php
+++ lib/connect.php
@@ -3,38 +3,49 @@ class DBQuestions {
     private static $mysqli;
 
     public static function connect(){
-            self::$mysqli = new mysqli("localhost", "root", "", "Questions");
-            self::$mysqli->set_charset("utf8");
-        }
+        self::$mysqli = new mysqli("localhost", "root", "", "Questions");
+        self::$mysqli->set_charset("utf8");
+    }
 
     public static function addQuestion($text, $name) {
-            $date = date('Y-m-d H:i:s');
-            $status = false;
-            $id = null;
-            return self::$mysqli->query("INSERT INTO questions (`ID`, `Text`, `Date`, `Status`, `Name`) VALUES ('$id', '$text', '$date', '$status', '$name')");
-        }
+        $date = date('Y-m-d H:i:s');
+        $status = 0;
+        $id = null;
+        $moderationDate = null;
+        return self::$mysqli->query("INSERT INTO questions (`ID`, `Text`, `Date`, `ModerationDate`, `Status`, `Name`) VALUES ('$id', '$text', '$date', '$moderationDate', '$status', '$name')");
+    }
 
     public static function getUnansweredQuestions(){
-        if ($result = self::$mysqli->query("SELECT * FROM  questions WHERE STATUS = 0 ORDER BY DATE")) {
+        if ($result = self::$mysqli->query("SELECT * FROM  questions WHERE STATUS = 1 ORDER BY DATE")) {
             return $result->fetch_all();
         }
     }
 
     public static function changeActiveQuestionStatus() {
-        return self::$mysqli->query("UPDATE questions SET Status = 1 WHERE STATUS = 0 ORDER BY DATE LIMIT 1");
+        return self::$mysqli->query("UPDATE questions SET Status = 2 WHERE Status = 1 ORDER BY ModerationDate LIMIT 1");
     }
 
     public static function hasUnansweredQuestions() {
-        $result = self::$mysqli->query("SELECT * FROM questions WHERE STATUS = 0");
-        $row_count = $result->num_rows;
-        if ($row_count == 0) {
+        if (self::$mysqli->query("SELECT * FROM questions WHERE STATUS = 1")->num_rows == 0) {
             return false;
-        }
-        else {
+        } else {
             return true;
         }
     }
 
+    public static function approveQuestion($id) {
+        if (self::$mysqli->query("SELECT * FROM questions WHERE ID = '$id'")->num_rows == 0) {
+            return false;
+        }
+        $moderationDate = date('Y-m-d H:i:s');
+        return self::$mysqli->query("UPDATE questions SET Status = 1, ModerationDate = '$moderationDate' WHERE ID = $id");
+    }
+
+    public static function removeQuestion($id) {
+        if (self::$mysqli->query("SELECT * FROM questions WHERE ID = '$id'")->num_rows == 0) {
+            return false;
+        }
+        return self::$mysqli->query("UPDATE questions SET Status = 2 WHERE ID = '$id'");
+    }
 }
-    DBQuestions::connect();
-?>
+DBQuestions::connect();
